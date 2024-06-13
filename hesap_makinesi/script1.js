b0e67abe8cb1845212f03eb3086c89bbf9268213
MelekOk <melek-ok@hotmail.com>
2024-06-13 22:02:13 +0300

    hesap-makinesi

---
+++ hesap_makinesi/script1.js
@@ -0,0 +1,30 @@
+﻿// Tüm hesap makinesi butonlarını seçer ve 'buttons' adlı bir değişkene atar
+let buttons = document.querySelectorAll(".button");
+// Hesap makinesi input'unu seçer ve 'display' adlı bir değişkene atar
+let display = document.querySelector(".display");
+
+// Her buton için bir tıklama olayı tanımlar
+buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+        // Tıklanan butonun değerini 'value' adlı bir değişkene atar
+        let value = button.textContent;
+        // Eğer tıklanan butonun değeri "C" ise
+        if (value === "C") {
+            // input'u temizler
+            display.value = "";
+            // Eğer tıklanan butonun değeri "=" ise
+        } else if (value === "=") {
+            try {
+                // input'daki ifadeyi hesaplar ve sonucu ekranda gösterir
+                display.value = eval(display.value);
+            } catch (e) {
+                // Hata oluşursa ekrana "Error" yazar
+                display.value = "Error";
+            }
+            // Diğer butonlar için (rakamlar ve operatörler)
+        } else {
+            // Tıklanan butonun değerini input'daki mevcut değerin sonuna ekler
+            display.value += value;
+        }
+    });
+});
